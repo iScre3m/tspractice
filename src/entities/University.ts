360e2e0b1ee673b1db630cfd9b801ce5d812f0c9
iScre3m <nahuel_1.5@outlook.com>
2023-12-15 18:40:58 -0300

    fixed entities with correct ref in objects

--- src/entities/University.ts
+++ src/entities/University.ts
@@ -3,13 +3,13 @@ import mongoose, {Document, Schema, Types} from 'mongoose'
 interface IUniversity extends Document{
     name: string;
     address: string;
-    coursesOffered: Types.ObjectId[];
+    courses: Types.ObjectId[];
 }
 
 const universitySchema = new Schema<IUniversity>({
     name: {type: String, required:true},
     address: {type: String, required:true},
-    coursesOffered: [{type: Schema.Types.ObjectId, ref: 'Courses'}]
+    courses: [{type: Schema.Types.ObjectId, ref: 'Course'}]
 })
 const University = mongoose.model<IUniversity>('University', universitySchema )
 export default University
