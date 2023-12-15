37ec59247a1c15f3d5233598ecb547577f7d12b7
iScre3m <nahuel_1.5@outlook.com>
2023-12-15 11:01:49 -0300

    Created route files

---
+++ src/routers/DepartmentRoute.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import DepartmentController from '../controllers/DepartmentController'
+
+const router = express.Router()
+
+router.get('/')
+router.get('/')
+router.post('/')
+router.put('/')
+router.delete('/')
---
+++ src/routers/StudentRoute.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import StudentController from '../controllers/StudentController'
+
+const router = express.Router()
+
+router.get('/')
+router.get('/')
+router.post('/')
+router.put('/')
+router.delete('/')
