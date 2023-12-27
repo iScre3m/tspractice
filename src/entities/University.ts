dc1ad3163e4b836b19905bb0f7357f16f9edc22a
iScre3m <nahuel_1.5@outlook.com>
2023-12-27 14:53:46 -0300

    Added degree enrollment exam entities

--- src/entities/University.ts
+++ src/entities/University.ts
@@ -4,12 +4,14 @@ interface IUniversity extends Document{
     name: string;
     address: string;
     courses: Types.ObjectId[];
+    departments: Types.ObjectId[];
 }
 
 const universitySchema = new Schema<IUniversity>({
     name: {type: String, required:true},
     address: {type: String, required:true},
-    courses: [{type: Schema.Types.ObjectId, ref: 'Course'}]
+    courses: [{type: Schema.Types.ObjectId, ref: 'Course'}], 
+    departments: [{type: Schema.Types.ObjectId, ref: 'Department'}]
 })
 const University = mongoose.model<IUniversity>('University', universitySchema )
 export default University
